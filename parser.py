eea57e2a0ccbdc8be2be491d5d02f4639660857a
junjora <kropotov.egor.stanislavovich@yandex.ru>
2020-12-04 16:39:01 +0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -13,19 +13,14 @@ def get_html(url):
 
 #ф-я для получения ссылок которые есть на страницу в ячейках таблиц
 def get_all_links(html):
-	
 	soup = BeautifulSoup(html, 'lxml') #(html code, parser) парсер, которые используется системой
-	
 	#list of objects Soup, parameters are html-tags
 	tds = soup.find('table', id = 'grid-data').find_all('td', class_='text-left')
-	
 	links = []
-	
 	for td in tds:
 		a = td.find('a').get('href') #string
 		link = 'http://dom.mingkh.ru/sverdlovskaya-oblast/ekaterinburg' + a
-		links.append(link)
-		
+		links.append(link)	
 	return links
 
 #ф-я для транслита слов
@@ -58,41 +53,36 @@ def get_page_data(html):
 	except:
 		dirty_data = ''
 	#СОЗДАНИЕ СПЛИТОВ ДЛЯ РАЗДЕЛЕНИЯ ДАННЫХ НА ПЕРЕМЕННЫЕ	
-	#address = dirty_data.split()[1]
-	
+	address = dirty_data.split()
 	#добавление переменных в список
 	data = {'name':var,
-			'price':price} #словарь
+		'price':price} #словарь
 	return data
-
 
 # lib XlsxWriter
 def export_to_excel(filename, data):
 	workbook = xlsxwriter.Workbook(filename)
 	worksheet = workbook.add_worksheet()
-	
 	#использование списка(кортеж), для заполнеия excel 
 	field_names = ('Адрес','','','','')
-	for i, var in enumerate(field_names):
-		
+	for i, var in enumerate(field_names):	
 	fields = ('','','','','')
 	for row,d in enumerate(data, start=1):
 		for col, field in enumerate(fields):
-			worksheet.write(row,col,d[field])
-			
+			worksheet.write(row,col,d[field])			
 		for teach in d['teachs']:
 			col += 1
 			worksheet.write(row,col,teach)
 	#worksheet.write(row,col,'content')
 	workbook.close()
+	
 '''   
 def write_csv(data):
 	with open('.csv','a') as f:
 		#объект писателя
 		writer = csv.writer(f)
-		
 		writer.writerow((data['name']),
-						data['price'])
+				data['price'])
 		print(data['name'], 'parsed')
 '''	
 
@@ -101,18 +91,15 @@ def make_all(url):
 	html = get_html(url)
 	data = get_page_data (html)
 	export_to_excel(data.xlxs,data)
-	
+
 def mail():
 	start = datetime.now()
-	
 	url = 'http://dom.mingkh.ru/sverdlovskaya-oblast/ekaterinburg/#stats'
 	#все сслыки
 	all_links = get_all_links(get_html(url))
-	
 	#for index, url in enumerate(all_links):
 	#	write_csv(get_page_data(get_html(url)))
 	#	print(index)
-	
 	
 	'''
 	for index, url in enumerate(all_links):
@@ -123,14 +110,11 @@ def mail():
 	'''
 	with Pool(40) as p:
 		#map(function, list_)
-		p.map(make_all,all_links)
-		
+		p.map(make_all,all_links)	
 	end = datetime.now()
-	
 	total = end - start
-	
 	print(str(total))
-	
+
 #точка входа
 if __name__ == '__main__':
   main()
