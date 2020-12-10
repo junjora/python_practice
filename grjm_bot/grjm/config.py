bab2fbe7340e44f0cc49eeea99706a32a53b90b4
junjora <kropotov.egor.stanislavovich@yandex.ru>
2020-12-10 11:05:44 +0500

    Create config.py

---
+++ grjm_bot/grjm/config.py
@@ -0,0 +1,29 @@
+TG_TOKEN = "" #получить у BotFather
+TG_API_URL = "https://telegg.ru/orig/bot"
+
+# Логирование
+LOGGING = {
+    'disable_existing_loggers': True,
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(module)s.%(funcName)s | %(asctime)s | %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+logging.config.dictConfig(LOGGING)
